c9791b6ca07e8498a602368087d4f53ab27af85b
Alejandro Leal <alejandro.leal05@outlook.com>
2022-07-22 16:19:50 -0500

    Guardado individual de archivos segun el canal

--- Server.go
+++ Server.go
@@ -14,6 +14,7 @@ func main() {
 
 func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
+		chn := r.FormValue("channels")                //captura el valor del canal seleccionado
 		file, handle, err := r.FormFile("fileClient") //obtenemos el archivo del formulario
 		if err != nil {
 			fmt.Fprint(w, "Error al cargar el archivo %v", err) //mostramos el error de carga al cliente
@@ -26,17 +27,18 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "Error al leer el archivo %v", err) //mostramos error de lectura al cliente
 			return
 		}
-
-		err = ioutil.WriteFile("./Files/"+handle.Filename, data, 0666) //guardamos el archivo en Files, "0666" -> permisos Unix
-		if err != nil {
-			fmt.Fprint(w, "Error al guardar el archivo %v", err)
+		if chn == "1" {
+			err = ioutil.WriteFile("./Channels/FilesCh1/"+handle.Filename, data, 0666) //guardamos el archivo en Files, "0666" -> permisos Unix
+			if err != nil {
+				fmt.Fprint(w, "Error al guardar el archivo %v", err)
+			}
+		} else {
+			err = ioutil.WriteFile("./Channels/FilesCh1/"+handle.Filename, data, 0666) //guardamos el archivo en Files, "0666" -> permisos Unix
+			if err != nil {
+				fmt.Fprint(w, "Error al guardar el archivo %v", err)
+			}
 		}
-
 		fmt.Fprint(w, "Cargado exitosamente!!!")
 
 	}
-}
-
-func servidor() {
-
 }
