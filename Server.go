b00ebc1d7cfa188d6f7e4bb8b695fdd8bd0c3c3e
Alejandro Leal <alejandro.leal05@outlook.com>
2022-07-22 12:00:39 -0500

    Cambios al servidor, modificado a http para envio de archivos

--- Server.go
+++ Server.go
@@ -2,42 +2,41 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	go servidor()
-	var input string
-	fmt.Scanln(&input)
+	http.Handle("/", http.FileServer(http.Dir("./public"))) //Creamos el servidor para archivos estaticos
+	http.HandleFunc("/upload", upload)                      //llamamos controlador de la ruta /upload
+	http.ListenAndServe(":8080", nil)                       //Servidor escuchando por el puerto 8080
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		file, handle, err := r.FormFile("fileClient") //obtenemos el archivo del formulario
+		if err != nil {
+			fmt.Fprint(w, "Error al cargar el archivo %v", err) //mostramos el error de carga al cliente
+			return
+		}
+		defer file.Close() //Cerramos el archivo
+
+		data, err := ioutil.ReadAll(file) //guardamos el archivo en data
+		if err != nil {
+			fmt.Fprint(w, "Error al leer el archivo %v", err) //mostramos error de lectura al cliente
+			return
+		}
+
+		err = ioutil.WriteFile("./Files/"+handle.Filename, data, 0666) //guardamos el archivo en Files, "0666" -> permisos Unix
+		if err != nil {
+			fmt.Fprint(w, "Error al guardar el archivo %v", err)
+		}
+
+		fmt.Fprint(w, "Cargado exitosamente!!!")
+
+	}
 }
 
 func servidor() {
-	s, err := net.Listen("tcp", ":3000")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	log.Println("Escuchando por el puerto localhost:3000")
-	for {
-		c, err := s.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		go handleClient(c)
-	}
-}
-
-func handleClient(c net.Conn) {
-	b := make([]byte, 100)
-	bs, err := c.Read(b)
-	if err != nil {
-		fmt.Println(err)
-		return
-	} else {
-		fmt.Println("Mensaje: ", b[:bs])
-		fmt.Println("Bytes: ", bs)
-	}
 
 }
